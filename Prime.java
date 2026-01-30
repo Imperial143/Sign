5e445c66287e3e36ea86fc5528234e2c44049788
Imperial <imperialgaming9805@gmail.com>
2026-01-30 06:50:08 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Prime {
+    public static void main(String[] args) {
+        int n = 5;
+        if (n <= 1) {
+            System.out.println("Not Prime");
+            return;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) {
+                System.out.println("Not Prime");
+                return;
+            }
+        System.out.println("Prime");
+    }
+}
