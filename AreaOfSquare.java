b0e750547274d6cf5cdad448aa672a7c9b4f8e87
Imperial <imperialgaming9805@gmail.com>
2026-01-30 06:47:31 +0530

    Create AreaOfSquare.java

---
+++ AreaOfSquare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        System.out.print("Enter the side of the square:5 ");
+        double side = 5;
+
+        double area = side * side;
+
+        System.out.println("Area of the square = " + area);
+
+        sc.close();
+    }
+}
