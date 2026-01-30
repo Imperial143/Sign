fc46ab0615d484995e5ce8d11c6b424385273e51
Imperial <imperialgaming9805@gmail.com>
2026-01-30 06:51:42 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class StringLength {
+    public static void main(String[] args) {
+        String s = "This is for jenkins";
+        System.out.println("Length = " + s.length());
+    }
+}
